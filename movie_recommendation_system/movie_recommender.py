e1a08fa243dfa3f3f5443ea307f48b7903531a86
Jakub Miętus <hidmir305@gmail.com>
2023-04-15 16:02:04 +0200

    Create recommend_movies function

--- movie_recommendation_system/movie_recommender.py
+++ movie_recommendation_system/movie_recommender.py
@@ -31,3 +31,17 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(movies["processed_description"])
 
 nearest_neighbors_model = NearestNeighbors(n_neighbors=5, metric="cosine")
 nearest_neighbors_model.fit(tfidf_matrix)
+
+
+def recommend_movies(description):
+    processed_input = process_description(description)
+    input_vector = tfidf_vectorizer.transform([processed_input])
+    distances, indices = nearest_neighbors_model.kneighbors(input_vector)
+
+    recommended_movies = []
+    for index in indices[0]:
+        movie_title = movies.iloc[index]["title"]
+        movie_description = movies.iloc[index]["description"]
+        recommended_movies.append((movie_title, movie_description))
+
+    return recommended_movies
