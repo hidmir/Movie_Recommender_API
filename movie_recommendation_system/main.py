c6a429c2ea8c94efc609b99ccf7c5117f1b4f979
Jakub Miętus <hidmir305@gmail.com>
2023-04-22 10:36:21 +0200

    Update Dockerfile and main.py

--- movie_recommendation_system/main.py
+++ movie_recommendation_system/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from typing import List
 from movie_recommender import get_movies_list, recommend_movies
+from os import environ
+import uvicorn
 
 app = FastAPI()
 
@@ -14,3 +16,7 @@ async def get_movies_list_endpoint():
 async def recommend_movies_endpoint(movie_description: str):
     recommended_movies = recommend_movies(movie_description)
     return {"recommended_movies": recommended_movies}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', port=int(environ.get("PORT", 5000)))
